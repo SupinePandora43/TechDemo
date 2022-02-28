0f36ce858f1546b3d5ef6719f5bb7a3b7d579d8a
SupinePandora43 <supinepandoraggg@gmail.com>
2022-02-28 23:19:28 +0500

    Reduncancy: format, add CreateShaderModule

--- src/AbstractVulkan/Redundancy.cs
+++ src/AbstractVulkan/Redundancy.cs
@@ -2,8 +2,9 @@ using Silk.NET.Vulkan;
 
 namespace AbstractVulkan;
 
-public static class Redundancy {
-    public static SampleCountFlags GetMaximumSamples(this SampleCountFlags sampleCountFlags) =>
+public static class Redundancy
+{
+	public static SampleCountFlags GetMaximumSamples(this SampleCountFlags sampleCountFlags) =>
 		sampleCountFlags.HasFlag(SampleCountFlags.SampleCount64Bit) ? SampleCountFlags.SampleCount64Bit
 		: sampleCountFlags.HasFlag(SampleCountFlags.SampleCount32Bit) ? SampleCountFlags.SampleCount32Bit
 		: sampleCountFlags.HasFlag(SampleCountFlags.SampleCount16Bit) ? SampleCountFlags.SampleCount16Bit
@@ -12,4 +13,10 @@ public static class Redundancy {
 		: sampleCountFlags.HasFlag(SampleCountFlags.SampleCount2Bit) ? SampleCountFlags.SampleCount2Bit
 		: sampleCountFlags.HasFlag(SampleCountFlags.SampleCount1Bit) ? SampleCountFlags.SampleCount1Bit
 		: 0;
+	public static unsafe Result CreateShaderModule(this Vk vk, Device device, byte* bytes, nuint length, out ShaderModule shaderModule)
+	{
+		ShaderModuleCreateInfo shaderModuleCI = new(codeSize: length, pCode: (uint*)bytes);
+		fixed (ShaderModule* shaderModulePtr = &shaderModule)
+			return vk.CreateShaderModule(device, &shaderModuleCI, null, shaderModulePtr);
+	}
 }
